0f6ee18ebd6cdcd948c3e670dc3c978f023e5cbe
Kevin White <InnocentSmith0934@users.noreply.github.com>
2019-01-16 23:31:28 -0600

    put a header and body tags around the generated pages. If a style.css file is present, link all output pages to it

--- process_md.sh
+++ process_md.sh
@@ -17,6 +17,33 @@ for x in $(find -type f -regex ".*\.md" -printf '%h\n'); do
   mkdir -p "${OUTPUT_DIR}${x/./}"
 done
 
+if [ -f style.css ]
+then
+  mkdir ${OUTPUT_DIR}/css
+  cp style.css ${OUTPUT_DIR}/css/
+  read -d '' HEADER << EOF
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+EOF
+else
+  read -d '' HEADER << EOF
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<body>
+EOF
+fi
+
 for x in $(find -type f -regex ".*\.md" -printf '%P\n'); do
-  cmark $x > ${OUTPUT_DIR}/${x/.md/.html}
+  FILE=${OUTPUT_DIR}/${x/.md/.html}
+  echo "$HEADER" > $FILE
+  cmark $x >> $FILE
+  echo "</body>" >> $FILE
 done
