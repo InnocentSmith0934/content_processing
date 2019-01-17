e60a0f73dcbca23077d9dbeeb24b9016abdf5835
Kevin White <InnocentSmith0934@users.noreply.github.com>
2019-01-17 00:06:55 -0600

    pretty up beginning and ending tags, update README to mention new behavior

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Will improve or replace with off-the-shelf solution as my requirements are disco
 ## Script(s) included:
 * **process_md.sh**\
   Finds all .md files in pwd and subdirectories, uses cmark to convert to html, writes file with same names but extension changed to .html\
+  Wraps content from .md file in a body tag and adds a doctype, lang, charset, and viewport settings.
+  If a file named style.css is included in pwd, the file is copied to a OUTPUT_DIR/css and the css file is linked in all output documents.
   Writes output to OUTPUT_DIR (default: __output), preserving relative directory structure.
 
 ## Installation
--- process_md.sh
+++ process_md.sh
@@ -17,6 +17,8 @@ for x in $(find -type f -regex ".*\.md" -printf '%h\n'); do
   mkdir -p "${OUTPUT_DIR}${x/./}"
 done
 
+# Determine header text based on presence of css file
+# If there is a css file, put it in a css directory and link it
 if [ -f style.css ]
 then
   mkdir ${OUTPUT_DIR}/css
@@ -24,9 +26,9 @@ then
   read -d '' HEADER << EOF
 <!DOCTYPE html>
 <html lang="en">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
@@ -35,15 +37,21 @@ else
   read -d '' HEADER << EOF
 <!DOCTYPE html>
 <html lang="en">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 <body>
 EOF
 fi
+read -d '' FOOTER << EOF
+</body>
+</html>
+EOF
 
 for x in $(find -type f -regex ".*\.md" -printf '%P\n'); do
   FILE=${OUTPUT_DIR}/${x/.md/.html}
   echo "$HEADER" > $FILE
   cmark $x >> $FILE
-  echo "</body>" >> $FILE
+  echo "$FOOTER" >> $FILE
 done
