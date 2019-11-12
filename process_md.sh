2173dbb21200194318c9c0608eb5ca488b6772a3
Kevin White <7097739+kevin-whitespace@users.noreply.github.com>
2019-11-11 23:26:55 -0500

    update header and footer for processing

--- process_md.sh
+++ process_md.sh
@@ -39,13 +39,19 @@ else
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  <title>The Timeline Project</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 EOF
 fi
 read -d '' FOOTER << EOF
 </body>
+<footer>
+<div id="footer_kayfabe"><p>&copy; 2122-25 The Centennial Foundation. All rights reserved, except where limited by the laws of the Hundred Worlds.</p><p>The Centennial Foundation was chartered in 2118 by the Trans-Lunar Treaty Organization (in cooperation with participating TLTO member nations, city-states, and self-governing zones), the Office of the Trade Representative of the Hundred Worlds, and the L3 Priory of the Ardán to preserve historically significant documents recording the first century of human-Stellan contact. The charter of the Centennial Foundation guarantees its complete editorial independence and academic freedom.</p></div>
+<div id="footer_real">Actually &copy; Copyright 2019 Kevin White. No aliens were consulted in the creation of these documents.</div>
+</footer>
 </html>
 EOF
 
